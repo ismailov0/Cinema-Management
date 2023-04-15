5e8122047198e932fde85a7559eab05fe75b785c
ismailov0 <aismailov0205@gmail.com>
2023-04-16 02:41:07 +0600

    Added docstrings and comments

--- main.py
+++ main.py
@@ -1,30 +1,40 @@
-from typing import List
-from pydantic import BaseModel
-
 from fastapi import FastAPI
 from middleware import RequestHandlingMiddleware
-from starlette.middleware.base import BaseHTTPMiddleware
-
-from services.cinemas import ManageCinema, Cinema
+from pydantic import BaseModel
 from services.afisha import ManageAfisha, SessionAfisha
+from services.cinemas import Cinema, ManageCinema
+from starlette.middleware.base import BaseHTTPMiddleware
+from typing import List
 
+
+# Create the FastAPI app
 app = FastAPI()
+
+# Define the middleware for handling exceptions
 exception_handler = RequestHandlingMiddleware()
+
+# Add the middleware to the app
 app.add_middleware(BaseHTTPMiddleware, dispatch=exception_handler)
 
 
 class CinemaResponse(BaseModel):
+    """
+    The response model for getting a list of cinemas.
+    """
     cinemas: List[Cinema]
 
 
 class AfishaResponse(BaseModel):
+    """
+    The response model for getting a list of afisha sessions.
+    """
     afisha: List[SessionAfisha]
 
 
 @app.get("/api/cinemas", response_model=CinemaResponse)
 def read_cinemas() -> CinemaResponse:
     """
-    Endpoint for retrieving cinema data from all cinemas.
+    Endpoint for retrieving all cinemas.
     """
     cinemas = ManageCinema().get_cinemas
     return CinemaResponse(cinemas=cinemas)
